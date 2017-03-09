c46a0dd33a24d6ed9156b6a2d87b2f81d510e0f4
Gigi Gloveli <gigigloveli2001@gmail.com>
2017-03-09 14:01:51 -0800

    added gitignore

--- build/web/WEB-INF/classes/ge/mziuri/resources/sql/SystemUserCreate.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE SystemUser (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
-    );

713ef467e3456569710f0cc246603b1d0db267f1
gio Shogiradze <giorgishogiradze@mziuri.ge>
2017-03-18 17:21:12 +0400

    SearchDao , SearchDaoImpl

---
+++ src/java/ge/mziuri/dao/SearchDAO.java
@@ -0,0 +1,9 @@
+
+package ge.mziuri.dao;
+
+import ge.mziuri.model.Item;
+
+
+public interface SearchDAO {
+    Item item(String Name) ;
+}
---
+++ src/java/ge/mziuri/dao/SearchDAOImpl.java
@@ -0,0 +1,37 @@
+package ge.mziuri.dao;
+
+import ge.mziuri.model.Item;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SearchDAOImpl implements SearchDAO {
+
+    private Connection con;
+
+    private PreparedStatement pstmt;
+
+    public SearchDAOImpl() {
+        con = DatabaseUtil.getConnection();
+    }
+
+    @Override
+    public Item item(String Name) {
+        try {
+            pstmt = con.prepareStatement("SELECT * FROM item  WHERE NAME=?");
+            pstmt.setString(1, Name);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Item item = new Item();
+                item.setName(rs.getString("name"));
+                item.setPhoto(rs.getString("photo"));
+                item.setPoint(rs.getInt("Point"));
+                return item;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+}

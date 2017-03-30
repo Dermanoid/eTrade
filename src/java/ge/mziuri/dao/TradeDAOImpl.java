ef94f16d07a2024a89488425a660f6dcf6cf7ea9
Gigi Gloveli <gigigloveli2001@gmail.com>
2017-03-30 23:44:24 +0400

    Checked Stuff after merge && Change variables in UAServlet

--- src/java/ge/mziuri/dao/TradeDAOImpl.java
+++ src/java/ge/mziuri/dao/TradeDAOImpl.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 package ge.mziuri.dao;
 
 import ge.mziuri.model.Item;
@@ -10,10 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-/**
- *
- * @author C4C2
- */
+ 
 public class TradeDAOImpl implements TradeDAO {
 
      private Connection con;
--- src/java/ge/mziuri/servlet/UAServlet.java
+++ src/java/ge/mziuri/servlet/UAServlet.java
@@ -31,12 +31,13 @@ public class UAServlet extends HttpServlet {
         String photo = request.getParameter("photo");
         String point = request.getParameter("Point");
         String Name = request.getParameter("name");
-
         Item item = new Item();
+        item.setPoint(0);
         item.setName(Name);
         item.setPoint(Integer.parseInt(point));
         item.setPhoto(photo);
         User user = new User();
+        //  int Rnd = item.getPoint() + (100 % 3) + 3 % 4 - 32;
         for (Cookie cookie : request.getCookies()) {
             if (cookie.getName().equals("logedInUserId")) {
                 user.setId(Integer.parseInt(cookie.getValue()));
@@ -45,46 +46,46 @@ public class UAServlet extends HttpServlet {
         item.setUser(user);
         switch (type) {
             case "ავეჯი":
-                item.setPoint(item.getPoint() + 10);
+                item.setPoint(item.getPoint() + 32);
                 break;
             case "ტექნიკა":
-                item.setPoint(item.getPoint() + 20);
+                item.setPoint(item.getPoint() + 29);
                 break;
             case "მანქანა":
-                item.setPoint(item.getPoint() + 30);
+                item.setPoint(item.getPoint() + 52);
                 break;
             case "მიწა":
-                item.setPoint(item.getPoint() + 40);
+                item.setPoint(item.getPoint() + 61);
                 break;
             case "სახლი":
-                item.setPoint(item.getPoint() + 50);
+                item.setPoint(item.getPoint() + 70);
                 break;
             case "წვრილმანი":
-                item.setPoint(item.getPoint() + 60);
+                item.setPoint(item.getPoint() + 8);
                 break;
 
         }
         int con = Integer.parseInt(Condition);
         if (con >= 0 && con <= 10) {
-            item.setPoint(item.getPoint() + 10);
+            item.setPoint(item.getPoint() + 1);
         } else if (con >= 11 && con <= 20) {
-            item.setPoint(item.getPoint() + 20);
+            item.setPoint(item.getPoint() + 4);
         } else if (con >= 21 && con <= 30) {
-            item.setPoint(item.getPoint() + 30);
+            item.setPoint(item.getPoint() + 6);
         } else if (con >= 31 && con <= 40) {
-            item.setPoint(item.getPoint() + 40);
+            item.setPoint(item.getPoint() + 11);
         } else if (con >= 41 && con <= 50) {
-            item.setPoint(item.getPoint() + 50);
+            item.setPoint(item.getPoint() + 15);
         } else if (con >= 51 && con <= 60) {
-            item.setPoint(item.getPoint() + 60);
+            item.setPoint(item.getPoint() + 19);
         } else if (con >= 61 && con <= 70) {
-            item.setPoint(item.getPoint() + 70);
+            item.setPoint(item.getPoint() + 22);
         } else if (con >= 71 && con <= 80) {
-            item.setPoint(item.getPoint() + 80);
+            item.setPoint(item.getPoint() + 27);
         } else if (con >= 81 && con <= 90) {
-            item.setPoint(item.getPoint() + 90);
+            item.setPoint(item.getPoint() + 30);
         } else if (con >= 91 && con <= 100) {
-            item.setPoint(item.getPoint() + 100);
+            item.setPoint(item.getPoint() + 39);
         };
 
         processRequest(request, response);

34d76e973d059b570f9046cf6f1a661aaf7632c2
giorgi <giorgigio740@gmail.com>
2017-05-06 16:05:00 +0400

    finished ItemDao , ItemDAOImpl

--- src/java/ge/mziuri/dao/ItemDAOImpl.java
+++ src/java/ge/mziuri/dao/ItemDAOImpl.java
@@ -1,11 +1,13 @@
 package ge.mziuri.dao;
 
 import ge.mziuri.model.Item;
+import ge.mziuri.model.User;
 import ge.mziuri.util.StringUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDAOImpl implements ItemDAO {
@@ -20,33 +22,74 @@ public class ItemDAOImpl implements ItemDAO {
 
     @Override
     public void addItem(Item item) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            pstmt = con.prepareStatement("INSERT INTO (name,description,photoes,owner_id,point,type) ITEM VALUES (?,?,?,?,?,?,?)");
+            pstmt.setString(1, item.getName());
+            pstmt.setString(2, item.getDescription());
+            pstmt.setString(3, StringUtil.getStringFromList(item.getPhotoes()));
+            pstmt.setInt(4, item.getUser().getId());
+            pstmt.setInt(5, item.getPoint());
+            pstmt.setString(6, item.getType().name());
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            DatabaseUtil.closeConnection(con);
+        }
     }
 
     @Override
     public List<Item> getAllItem() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        List<Item> items = new ArrayList<>();
 
-    @Override
-    public List<Item> getAllItemByName(String name) {
         try {
-            pstmt = con.prepareStatement("SELECT * FROM item  WHERE NAME=?");
-            pstmt.setString(1, name);
+            pstmt = con.prepareStatement("SELECT * FROM item  ");
             ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
+            while (rs.next()) {
+                User user = new User();
                 Item item = new Item();
+                item.setId(rs.getInt("id"));
                 item.setName(rs.getString("name"));
-                String m = rs.getString("Photo");
-                item.setPhotoes(StringUtil.getStringListFromString(m));
+                item.setDescription(rs.getString("description"));
+                item.setPhotoes(StringUtil.getStringListFromString(rs.getString("Photo")));
+                user.setId(rs.getInt("owner_id"));
+                item.setUser(user);
                 item.setPoint(rs.getInt("Point"));
-//                return item;
+                items.add(item);
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         } finally {
             DatabaseUtil.closeConnection(con);
         }
-        return null;
+        return items;
+    }
+
+    @Override
+    public List<Item> getAllItemByName(String name) {
+        List<Item> items = new ArrayList<>();
+
+        try {
+            pstmt = con.prepareStatement("SELECT * FROM item WHERE NAME=? ");
+            pstmt.setString(1, name);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                User user = new User();
+                Item item = new Item();
+                item.setId(rs.getInt("id"));
+                item.setName(rs.getString("name"));
+                item.setDescription(rs.getString("description"));
+                item.setPhotoes(StringUtil.getStringListFromString(rs.getString("Photo")));
+                user.setId(rs.getInt("owner_id"));
+                item.setUser(user);
+                item.setPoint(rs.getInt("Point"));
+                items.add(item);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            DatabaseUtil.closeConnection(con);
+        }
+        return items;
     }
 }

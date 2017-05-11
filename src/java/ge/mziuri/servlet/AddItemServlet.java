a45feb291aa349ac5dc554f6e4f94f366c86ef78
Gigi Gloveli <gigigloveli2001@gmail.com>
2017-05-11 18:33:27 +0400

    Completed Photo Uploading

--- src/java/ge/mziuri/servlet/AddItemServlet.java
+++ src/java/ge/mziuri/servlet/AddItemServlet.java
@@ -3,18 +3,39 @@ package ge.mziuri.servlet;
 import ge.mziuri.model.Item;
 import ge.mziuri.model.User;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import ge.mziuri.util.StringUtil;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Random;
+import javax.servlet.http.Cookie;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
 
 @WebServlet(name = "UAServlet", urlPatterns = {"/UAServlet"})
 public class AddItemServlet extends HttpServlet {
+
+    private boolean isMultipart;
+    private String filePath;
+    private int maxFileSize = 50000 * 1024;
+    private int maxMemSize = 50000 * 1024;
+    private File file;
+
+    private String imagePath;
+
+    public void init() {
+
+        filePath = getServletContext().getInitParameter("file-upload");
+    }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -33,7 +54,7 @@ public class AddItemServlet extends HttpServlet {
         String type = request.getParameter("Types");
         String photo1 = request.getParameter("photo1");
         String photo2 = request.getParameter("photo2");
-        String photo3 = request.getParameter("photo3");  
+        String photo3 = request.getParameter("photo3");
         String point = request.getParameter("Point");
         String Name = request.getParameter("name");
         List<String> photos = new ArrayList<>();
@@ -44,7 +65,7 @@ public class AddItemServlet extends HttpServlet {
         item.setPoint(0);
         item.setName(Name);
         item.setPoint(Integer.parseInt(point));
-        item.setPhotoes(photos); 
+        item.setPhotoes(photos);
         User user = new User();
         // VVX  int Rnd = item.getPoint() + (100 % 3) + 3 % 4 - 32;
         for (Cookie cookie : request.getCookies()) {
@@ -95,79 +116,60 @@ public class AddItemServlet extends HttpServlet {
             item.setPoint(item.getPoint() + 30);
         } else if (con >= 91 && con <= 100) {
             item.setPoint(item.getPoint() + 39);
-        };
-//   try {
-//
-//            isMultipart = ServletFileUpload.isMultipartContent(request);
-//            response.setContentType("text/html");
-//
-//            DiskFileItemFactory factory = new DiskFileItemFactory();
-//            // maximum size that will be stored in memory
-//            factory.setSizeThreshold(maxMemSize);
-//            // Location to save data that is larger than maxMemSize.
-//            factory.setRepository(new File("c:\\temp"));
-//
-//            // Create a new file upload handler
-//            ServletFileUpload upload = new ServletFileUpload(factory);
-//            // maximum file size to be uploaded.
-//            upload.setSizeMax(maxFileSize);
-//
-//            // Parse the request to get file items.
-//            List fileItems = upload.parseRequest(request);
-//
-//            // Process the uploaded file items
-//            Iterator i = fileItems.iterator();
-//            Random random = new Random();
-//
-//            while (i.hasNext()) {
-//                FileItem fi = (FileItem) i.next();
-//                if (!fi.isFormField()) {
-//
-//                    String fieldName = fi.getName() + " " + fi.getFieldName();
-//                    String fileName = fi.getName();
-//                    String contentType = fi.getContentType();
-//                    boolean isInMemory = fi.isInMemory();
-//                    long sizeInBytes = fi.getSize();
-//
-//                    String extention = FilenameUtils.getExtension(fileName);
-//                    String imageName = FilenameUtils.removeExtension(fileName);
-//
-//                    if (fileName.lastIndexOf("\\") >= 0) {
-//                        fileName = imageName + random.nextInt() + "." + extention;
-//                        imagePath = fileName;
-//
-//                        file = new File(filePath
-//                                + fileName.substring(fileName.lastIndexOf("\\")));
-//
-//                    } else {
-//
-//                        fileName = imageName + random.nextInt() + "." + extention;
-//                        imagePath = fileName;
-//
-//                        file = new File(filePath
-//                                + fileName.substring(fileName.lastIndexOf("\\") + 1));
-//
-//                    }
-//                    try {
-//                        fi.write(file);
-//                    } catch (Exception ex) {
-//                        Logger.getLogger(addFoodServlet.class.getName()).log(Level.SEVERE, null, ex);
-//                    }
-//
-//                }
-//            }
-//
-//            Hashtable table = new Hashtable();
-//
-//            Iterator<FileItem> iter = fileItems.iterator();
-//            while (iter.hasNext()) {
-//
-//                FileItem item = iter.next();
-//                if (!item.getFieldName().equals("file")) {
-//                    table.put(item.getFieldName(), item.getString());
-//
-//                }
-//            }
-        processRequest(request, response);
+        }
+        try {
+            isMultipart = ServletFileUpload.isMultipartContent(request);
+            response.setContentType("text/html");
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+            // maximum size that will be stored in memory
+            factory.setSizeThreshold(maxMemSize);
+            // Location to save data that is larger than maxMemSize.
+            factory.setRepository(new File("c:\\temp"));
+            // Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            // maximum file size to be uploaded.
+            upload.setSizeMax(maxFileSize);
+            // Parse the request to get file items.
+            List fileItems = upload.parseRequest(request);
+            // Process the uploaded file items
+            Iterator i = fileItems.iterator();
+            Random random = new Random();
+            while (i.hasNext()) {
+                FileItem fi = (FileItem) i.next();
+                if (!fi.isFormField()) {
+                    String fieldName = fi.getName() + " " + fi.getFieldName();
+                    String fileName = fi.getName();
+                    String contentType = fi.getContentType();
+                    boolean isInMemory = fi.isInMemory();
+                    long sizeInBytes = fi.getSize();
+                    String extention = FilenameUtils.getExtension(fileName);
+                    String imageName = FilenameUtils.removeExtension(fileName);
+                    if (fileName.lastIndexOf("\\") >= 0) {
+                        fileName = imageName + random.nextInt() + "." + extention;
+                        imagePath = fileName;
+                        file = new File(filePath + fileName.substring(fileName.lastIndexOf("\\")));
+                    } else {
+                        fileName = imageName + random.nextInt() + "." + extention;
+                        imagePath = fileName;
+                        file = new File(filePath + fileName.substring(fileName.lastIndexOf("\\") + 1));
+                    }
+                    try {
+                        fi.write(file);
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+            Hashtable table = new Hashtable();
+            Iterator<FileItem> iter = fileItems.iterator();
+            while (iter.hasNext()) {
+                FileItem item1 = iter.next();
+                if (!item1.getFieldName().equals("file")) {
+                    table.put(item1.getFieldName(), item1.getString());
+                }
+            }
+            processRequest(request, response);
+        } catch (FileUploadException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
---
+++ web/addItem.jsp
@@ -0,0 +1,54 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="public/css/style.css">
+        <title>ნივთის დამატება</title>
+    </head>
+    <body>
+        <div class="UAForm">
+            <form action="AddItemServlet" method="POST" enctype="multipart/form-data">
+                <h5 style ="display: inline-block ">სახელი : </h5>  <input type="text" name="Name" /> <br>
+                <h5 style ="display: inline-block ">ნივთის ტიპი :</h5> 
+                <select name="Types">
+                    <option value="FURNITURE">ავეჯი</option>
+                    <option value="TECH">ტექნიკა</option>
+                    <option value="CAR">მანქანა</option>
+                    <option value="LITLE_STUFF">წვრილმანი</option>
+                    <option value="GROUND">მიწა</option>
+                    <option value="HOUSE">სახლი</option> 
+                </select> <br>
+                <h5 style ="display: inline-block">
+                    ნივთის მდგომარეობა :</h5>  <input type="number" name="Condition" min="1" max="100"><br>
+                <h5>ტექსტის ინფორმაცია : </h5>
+                <textarea  rows="5" cols="5">
+
+                </textarea> <br>
+                <h5>დაამატეთ ფოტო : </h5> <img id="inp1" width="320" height="200"  src="Photo" alt="Error: 404" /> <img width="320" height="200" id="inp2" src="Photo" alt="Error: 404" /> <img width="320" height="200" id="inp3" src="Photo" alt="Error: 404" /> <br>
+                <input id="FileUpload" style="margin-bottom: 20px; margin-left: 90px" type="file" name="file" accept="image/*" onchange="loadFile(event)" multiple  > <br>
+                <script>
+                    var loadFile = function (event) {
+                        var output = document.getElementById('inp1');
+                        output.src = URL.createObjectURL(event.target.files[0]);
+                        var output = document.getElementById('inp2');
+                        output.src = URL.createObjectURL(event.target.files[1]);
+                        var output = document.getElementById('inp3');
+                        output.src = URL.createObjectURL(event.target.files[2]);
+                        var files = document.getElementById('FileUpload').files;
+                        if (files.length > 3) {
+                            alert("Files count is more then 3");
+                          executeScript();
+                        }
+                    };
+                </script>        
+                <input  type="submit" value="ნივთის დამატება" />
+            </form>
+        </div>
+    </body>
+</html>
+
+
+
